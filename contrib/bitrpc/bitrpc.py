9c98111cd616f4396c67f066355319d554102b91
Eliqa dev <worker@worker>
2018-04-07 18:32:04 +0200

    Update rpc port

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -11,9 +11,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-    access = ServiceProxy("http://127.0.0.1:51473")
+    access = ServiceProxy("http://127.0.0.1:52273")
 else:
-    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:51473")
+    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:52273")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
